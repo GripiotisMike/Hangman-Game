819447ed91d2b8a1a13d65584f91871bc84d7d48
GripiotisMike <gripiotism@yahoo.com>
2024-11-16 16:13:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,60 @@
-stages = ['''
-  +---+
+import random
+
+# Hangman stages representing the player's life status
+stages = [
+    '''  +---+
   |   |
   O   |
  /|\  |
  / \  |
       |
-=========
-''', '''
-  +---+
+=========''',
+    '''  +---+
   |   |
   O   |
  /|\  |
  /    |
       |
-=========
-''', '''
-  +---+
+=========''',
+    '''  +---+
   |   |
   O   |
  /|\  |
       |
       |
-=========
-''', '''
-  +---+
+=========''',
+    '''  +---+
   |   |
   O   |
  /|   |
       |
       |
-=========''', '''
-  +---+
+=========''',
+    '''  +---+
   |   |
   O   |
   |   |
       |
       |
-=========
-''', '''
-  +---+
+=========''',
+    '''  +---+
   |   |
   O   |
       |
       |
       |
-=========
-''', '''
-  +---+
+=========''',
+    '''  +---+
   |   |
       |
       |
       |
       |
-=========
-''']
+========='''
+]
 
-logo = ''' 
+# Hangman logo
+logo = '''
  _                                             
 | |                                            
 | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
@@ -63,259 +62,65 @@ logo = '''
 | | | | (_| | | | | (_| | | | | | | (_| | | | |
 |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                     __/ |                      
-                   |___/    '''
+                   |___/    
+'''
+
+# Word list
 word_list = [
-'abruptly', 
-'absurd', 
-'abyss', 
-'affix', 
-'askew', 
-'avenue', 
-'awkward', 
-'axiom', 
-'azure', 
-'bagpipes', 
-'bandwagon', 
-'banjo', 
-'bayou', 
-'beekeeper', 
-'bikini', 
-'blitz', 
-'blizzard', 
-'boggle', 
-'bookworm', 
-'boxcar', 
-'boxful', 
-'buckaroo', 
-'buffalo', 
-'buffoon', 
-'buxom', 
-'buzzard', 
-'buzzing', 
-'buzzwords', 
-'caliph', 
-'cobweb', 
-'cockiness', 
-'croquet', 
-'crypt', 
-'curacao', 
-'cycle', 
-'daiquiri', 
-'dirndl', 
-'disavow', 
-'dizzying', 
-'duplex', 
-'dwarves', 
-'embezzle', 
-'equip', 
-'espionage', 
-'euouae', 
-'exodus', 
-'faking', 
-'fishhook', 
-'fixable', 
-'fjord', 
-'flapjack', 
-'flopping', 
-'fluffiness', 
-'flyby', 
-'foxglove', 
-'frazzled', 
-'frizzled', 
-'fuchsia', 
-'funny', 
-'gabby', 
-'galaxy', 
-'galvanize', 
-'gazebo', 
-'giaour', 
-'gizmo', 
-'glowworm', 
-'glyph', 
-'gnarly', 
-'gnostic', 
-'gossip', 
-'grogginess', 
-'haiku', 
-'haphazard', 
-'hyphen', 
-'iatrogenic', 
-'icebox', 
-'injury', 
-'ivory', 
-'ivy', 
-'jackpot', 
-'jaundice', 
-'jawbreaker', 
-'jaywalk', 
-'jazziest', 
-'jazzy', 
-'jelly', 
-'jigsaw', 
-'jinx', 
-'jiujitsu', 
-'jockey', 
-'jogging', 
-'joking', 
-'jovial', 
-'joyful', 
-'juicy', 
-'jukebox', 
-'jumbo', 
-'kayak', 
-'kazoo', 
-'keyhole', 
-'khaki', 
-'kilobyte', 
-'kiosk', 
-'kitsch', 
-'kiwifruit', 
-'klutz', 
-'knapsack', 
-'larynx', 
-'lengths', 
-'lucky', 
-'luxury', 
-'lymph', 
-'marquis', 
-'matrix', 
-'megahertz', 
-'microwave', 
-'mnemonic', 
-'mystify', 
-'naphtha', 
-'nightclub', 
-'nowadays', 
-'numbskull', 
-'nymph', 
-'onyx', 
-'ovary', 
-'oxidize', 
-'oxygen', 
-'pajama', 
-'peekaboo', 
-'phlegm', 
-'pixel', 
-'pizazz', 
-'pneumonia', 
-'polka', 
-'pshaw', 
-'psyche', 
-'puppy', 
-'puzzling', 
-'quartz', 
-'queue', 
-'quips', 
-'quixotic', 
-'quiz', 
-'quizzes', 
-'quorum', 
-'razzmatazz', 
-'rhubarb', 
-'rhythm', 
-'rickshaw', 
-'schnapps', 
-'scratch', 
-'shiv', 
-'snazzy', 
-'sphinx', 
-'spritz', 
-'squawk', 
-'staff', 
-'strength', 
-'strengths', 
-'stretch', 
-'stronghold', 
-'stymied', 
-'subway', 
-'swivel', 
-'syndrome', 
-'thriftless', 
-'thumbscrew', 
-'topaz', 
-'transcript', 
-'transgress', 
-'transplant', 
-'triphthong', 
-'twelfth', 
-'twelfths', 
-'unknown', 
-'unworthy', 
-'unzip', 
-'uptown', 
-'vaporize', 
-'vixen', 
-'vodka', 
-'voodoo', 
-'vortex', 
-'voyeurism', 
-'walkway', 
-'waltz', 
-'wave', 
-'wavy', 
-'waxy', 
-'wellspring', 
-'wheezy', 
-'whiskey', 
-'whizzing', 
-'whomever', 
-'wimpy', 
-'witchcraft', 
-'wizard', 
-'woozy', 
-'wristwatch', 
-'wyvern', 
-'xylophone', 
-'yachtsman', 
-'yippee', 
-'yoked', 
-'youthful', 
-'yummy', 
-'zephyr', 
-'zigzag', 
-'zigzagging', 
-'zilch', 
-'zipper', 
-'zodiac', 
-'zombie', 
+    'abruptly', 'absurd', 'abyss', 'affix', 'askew', 'avenue', 'awkward', 'axiom', 'azure', 'bagpipes',
+    'bandwagon', 'banjo', 'bayou', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm',
+    'buzzard', 'caliph', 'cobweb', 'cycle', 'daiquiri', 'dizzying', 'duplex', 'embezzle', 'equip',
+    'exodus', 'fuchsia', 'galaxy', 'gizmo', 'haiku', 'ivory', 'jackpot', 'jigsaw', 'jinx', 'juicy',
+    'jukebox', 'kayak', 'kilobyte', 'luxury', 'mystify', 'onyx', 'oxygen', 'pajama', 'pixel', 'quartz',
+    'quiz', 'quizzes', 'rhubarb', 'sphinx', 'strength', 'twelfth', 'unknown', 'vaporize', 'vodka',
+    'waltz', 'whizzing', 'wizard', 'xylophone', 'yachtsman', 'zephyr', 'zodiac', 'zombie'
 ]
 
-import random
+def hangman():
+    """Main function to play Hangman."""
+    print(logo)
+    
+    # Randomly select a word
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
 
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
+    # Initialize variables
+    display = ["_"] * word_length
+    lives = len(stages) - 1
+    guessed_letters = []
+    end_of_game = False
 
-end_of_game = False
-lives = 6
+    # Main game loop
+    while not end_of_game:
+        guess = input("Guess a letter: ").lower()
 
-print (logo)
+        # Check for repeated guesses
+        if guess in guessed_letters:
+            print(f"You've already guessed '{guess}'. Try again!")
+            continue
+        guessed_letters.append(guess)
 
-display = []
-for _ in range(word_length):
-    display += "_"
-guessl = []
-while not end_of_game:
-    guess = input("Guess a letter: ").lower()
-    if guess in guessl:
-      print ("You have already tried this letter.")
-      continue
-    guessl.append(guess)
-  
-    for position in range(word_length):
-        letter = chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-          
-    if guess not in chosen_word:
-        print ("Sorry but there is no {} in this word!".format(guess))
-        lives -= 1
-        if lives == 0:
+        # Update the display if the guess is correct
+        if guess in chosen_word:
+            for position in range(word_length):
+                if chosen_word[position] == guess:
+                    display[position] = guess
+        else:
+            print(f"Sorry, '{guess}' is not in the word.")
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print("You lose!")
+                print(f"The word was: {chosen_word}")
+        
+        # Show progress
+        print(f"{' '.join(display)}")
+        print(stages[lives])
+
+        # Check for a win
+        if "_" not in display:
             end_of_game = True
-            print("You lose.")
-            print ("The word was : {}".format(chosen_word))
-    print(f"{' '.join(display)}")
-  
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
-    print(stages[lives])
+            print("Congratulations, you win!")
+
+if __name__ == "__main__":
+    hangman()
